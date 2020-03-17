12ea42babaafc94846cf0f558c4bf78906c106a4
Grigoriy mikirtumov <mikigrigorij@yandex.ru>
2020-03-17 11:07:46 +0300

    homework-2

--- index.php
+++ index.php
@@ -1,3 +1,10 @@
+<?php 
+    $_title="minimalistica";
+    $_h1="minimalistica"; 
+    $_year=date('Y');
+    ?>
+
+
 
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
@@ -5,55 +12,17 @@
 	<meta http-equiv="content-type" content="text/html;charset=utf-8" />
 	<meta name="author" content="Luka Cvrk (www.solucija.com)" />
 	<link rel="stylesheet" href="css/main.css" type="text/css" />
-	<title>minimalistica</title>
+	<title><?=$_title?></title> 
 </head>
 <body>
-	<?php
-	$menu = "<ul id='menu'>";
-	$menu .="<li><a href='#'>home</a></li>";
-	$menu .="<li><a href='#'>archive</a></li>";
-	$menu .="<li><a href='#'>contact</a></li>";
-	$menu.="</ul>";
 
-	$post="<div class='post'>";
-	$postDetails="<div class='details'>";
-	$postDetails.="<h2><a href='#'>Nunc commodo euismod massa quis vestibulum</a></h2>";
-	$postDetails.="<p class='info'>posted 3 hours ago in <a href='#'>general</a></p>";
-	$postDetails.="</div>";
-	$postBody = "<div class='body'>";
-	$postBody.= "<p>Nunc eget nunc libero. Nunc commodo euismod massa quis vestibulum. Proin mi nibh, dignissim a pellentesque at, ultricies sit amet sapien. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque vel lorem eu libero laoreet facilisis. Aenean placerat, ligula quis placerat iaculis, mi magna luctus nibh, adipiscing pretium erat neque vitae augue. Quisque consectetur odio ut sem semper commodo. Maecenas iaculis leo a ligula euismod condimentum. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Ut enim risus, rhoncus sit amet ultricies vel, aliquet ut dolor. Duis iaculis urna vel massa ultricies suscipit. Phasellus diam sapien, fermentum a eleifend non, luctus non augue. Quisque scelerisque purus quis eros sollicitudin gravida. Aliquam erat volutpat. Donec a sem consequat tortor posuere dignissim sit amet at ipsum.</p>";
-	$postBody.="</div>";
-	$postX = "<div class='x'></div>";
-	$post.=$postDetails.$postBody.$postX."</div>";
-
-	$colTitleContent1 = "Ut enim risus rhoncus";
-	$colTitleContent2 = "Maecenas iaculis leo";
-	$colTitleContent3 = "Quisque consectetur odio";
-	$colTitle = "<h3><a href='#'>";
-	$colDescription = "<p>Quisque consectetur odio ut sem semper commodo. Maecenas iaculis leo a ligula euismod condimentum. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Ut enim risus, rhoncus sit amet ultricies vel, aliquet ut dolor. Duis iaculis urna vel massa ultricies suscipit. Phasellus diam sapien, fermentum a eleifend non, luctus non augue. Quisque scelerisque purus quis eros sollicitudin gravida. Aliquam erat volutpat. Donec a sem consequat tortor posuere dignissim sit amet at.</p>";
-	$colLink = "<p>&not; <a href='#'>read more</a></p>";
-	$colTitle1=$colTitle.$colTitleContent1."</a></h3>";
-	$colTitle2=$colTitle.$colTitleContent2."</a></h3>";
-	$colTitle3=$colTitle.$colTitleContent3."</a></h3>";
-	$col1="<div class='col'>".$colTitle1.$colDescription.$colLink."</div>";
-	$col2="<div class='col'>".$colTitle2.$colDescription.$colLink."</div>";
-	$col3="<div class='col last'>".$colTitle3.$colDescription.$colLink."</div>";
-
-	$footer = "<div id='footer'>";
-	$footerContent = "<p>Copyright &copy; <em>minimalistica</em> &middot; Design: Luka Cvrk, <a href='http://www.solucija.com/' title='Free CSS Templates'>Solucija</a></p>";
-	$footer.=$footerContent."</div>";
-
-	$content="<div id='content'>".$menu.$post.$col1.$col2.$col3.$footer."</div>";
-	echo $content;
-	?>
-	<!-- <div id="content">
-		<h1></h1>
+	<div id="content">
+		<h1><?=$_h1?></h1>
 		
 		<ul id="menu">
 			<li><a href="#">home</a></li>
 			<li><a href="#">archive</a></li>
 			<li><a href="#">contact</a></li>
-			
 		</ul>
 	
 		<div class="post">
@@ -85,8 +54,9 @@
 		</div>
 		
 		<div id="footer">
-			<p>Copyright &copy; <em>minimalistica</em> &middot; Design: Luka Cvrk, <a href="http://www.solucija.com/" title="Free CSS Templates">Solucija</a></p>
+			<p>Copyright &copy; <?=$_year; ?> ><em>minimalistica</em> &middot; Design: Luka Cvrk, <a href="http://www.solucija.com/" title="Free CSS Templates">Solucija</a></p>
+			<span>Год</span> <?= date("Y") ?>
 		</div>	
-	</div> -->
+	</div>
 </body>
 </html>
