1057fd358f01d12c50fa872d709c1ff4f178ebb7
Grigoriy mikirtumov <mikigrigorij@yandex.ru>
2020-04-01 15:20:41 +0300

    homework-5

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-
+<!-- 
 <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
 <script>
     function f(id){
@@ -13,18 +13,39 @@
             }
         })
     }
-</script>
+</script> -->
 
 
-<?php 
-    include "config.php";
-    $sql = "select * from shop";
-    $res = mysqli_query($connect, $sql);
-    $form='<table border="1" width="400">';
-    while($data = mysqli_fetch_assoc($res)){
-        //echo "Автомобиль ".$data['name']." стоит ".$data['price']."<br>";
-        $form.="<tr><td>".$data['name']."</td><td><input value=".$data['price']." type='text' id=".$data['id']."></td><td><input type='button' value='Сохранить' onclick='f(".$data['id'].")'></td></tr>";
-    }
-    $form.="</table>";
-    echo $form;
-?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://fonts.googleapis.com/css?family=Pacifico&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="./style/normalize.css">
+    <link rel="stylesheet" href="./style/style.css">
+    <title>Cotogramm</title>
+</head>
+<body>
+    <?php include "./block/header.php" ?>
+    <?php include "./block/galary.php" ?>
+    <?php include "./block/footer.php" ?>
+    <script src="https://kit.fontawesome.com/2426fc9e9e.js" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+    <script>
+       function addViews(id){
+            let str = "id="+id;
+            $.ajax({
+                type:"GET",
+                url:"server.php",
+                data: str,
+                success: function(x){
+                    alert(x)
+                }
+            })
+       }
+    </script>
+</body>
+</html>
--- server.php
+++ server.php
@@ -1,8 +1,8 @@
 <?php 
     include "config.php";
     $id = $_GET['id'];
-    $price = $_GET['price'];
-    $sql = "update shop set price=$price where id=$id" ;
+    $sql = "update shop set views=views+1 where id=$id" ;
+    //UPDATE `table_name` SET `field` = `field` + 1
     if(mysqli_query($connect,$sql)){
         echo "Успех";
     }else{
