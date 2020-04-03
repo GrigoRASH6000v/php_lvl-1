81abd32b566f453ba8c8fafe19eda2bf5c33bfb0
Grigoriy mikirtumov <mikigrigorij@yandex.ru>
2020-04-03 15:04:39 +0300

    homework-7

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php 
     const SERVER = "localhost";
-    const DB = "homework-5";
+    const DB = "homework-6";
     const LOGIN ="root";
     const PASS = "";
     $connect = mysqli_connect(SERVER, LOGIN, PASS, DB) or die ("Ошибка соединения с базой")
--- index.php
+++ index.php
@@ -1,51 +1,19 @@
-<!-- 
-<script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
-<script>
-    function f(id){
-        let price = document.getElementById(id).value
-        let str = "price="+price+"&id="+id;
-        $.ajax({
-            type:"GET",
-            url:"server.php",
-            data: str,
-            success: function(x){
-                alert(x)
-            }
-        })
-    }
-</script> -->
-
-
-
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://fonts.googleapis.com/css?family=Pacifico&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="./style/normalize.css">
-    <link rel="stylesheet" href="./style/style.css">
-    <title>Cotogramm</title>
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="./css/normalize.css">
+    <link rel="stylesheet" href="./css/style-2.css">
+    <title>Market</title>
 </head>
 <body>
-    <?php include "./block/header.php" ?>
-    <?php include "./block/galary.php" ?>
-    <?php include "./block/footer.php" ?>
-    <script src="https://kit.fontawesome.com/2426fc9e9e.js" crossorigin="anonymous"></script>
-    <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
-    <script>
-       function addViews(id){
-            let str = "id="+id;
-            $.ajax({
-                type:"GET",
-                url:"server.php",
-                data: str,
-                // success: function(x){
-                //     alert(x)
-                // }
-            })
-       }
-    </script>
+    <div class="container">
+       <?php include "./blocks/header.php" ?>
+       <?php include "./blocks/main.php" ?>
+       <footer></footer>
+    </div>
+    <!-- <script src='js/script.js'></script> -->
 </body>
 </html>
